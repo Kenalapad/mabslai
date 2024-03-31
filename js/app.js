9961cba365efa8c26cdb6a06089cd46cef61b06f
Kenneth Lester Alapad <89431616+Kenalapad@users.noreply.github.com>
2024-03-31 16:48:46 +0800

    Uploading all resource file

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+// Initialization for ES Users
+import { Input, initMDB } from "mdb-ui-kit";
+
+initMDB({ Input });
+
+// Initialization for ES Users
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown, Collapse });
+
+
+// Initialization for ES Users
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+initMDB({ Carousel });
+
+
+// Initialization for ES Users
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown, Collapse });
