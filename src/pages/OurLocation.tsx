7f3fadbd3358fa19c9947dab4ec55760bbd9680c
Ken Alapad <alapadkennethlester@gmail.com>
2025-07-26 13:17:39 +0800

    Updating the images

--- src/pages/OurLocation.tsx
+++ src/pages/OurLocation.tsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import mabslai from "../images/mabslai.png";
+import fbLogo from "../images/fb-logo.jpg";
+import facebookSvg from "../images/logo/facebook.svg";
+import instagramSvg from "../images/logo/instagram.svg";
+import whatsappSvg from "../images/logo/whatsapp.svg";
+import youtubeSvg from "../images/logo/youtube-logo.jpg";
+import mabslaiLogo from "../images/mabslai.png";
+import email from "../images/logo/gmail.png";
+import phone from "../images/logo/phone.png";
+import dialphone from "../images/logo/dialphone.png";
 
 function OurLocation() {
   // NavBar
@@ -88,7 +98,7 @@ function OurLocation() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
         <div className="container-fluid">
           <img
-            src="/src/images/mabslai.png"
+            src={mabslai}
             style={{ width: "80px" }}
             alt="mabslaiLogo"
             onClick={() => navigate("/Home")}
@@ -346,7 +356,7 @@ function OurLocation() {
           <div className="d-flex justify-content-end">
             <div className="row">
               <a className="me-4 text-rest " href="#">
-                <img src="/src/images/logo/fb-logo.jpg" alt="" />
+                <img src={fbLogo} alt="" />
               </a>
             </div>
           </div>
@@ -368,11 +378,7 @@ function OurLocation() {
               className="me-4 text-reset"
               style={{ textDecoration: "none" }}
             >
-              <img
-                src="/src/images/logo/facebook.svg"
-                alt="facebook"
-                style={{ width: "34px" }}
-              />
+              <img src={facebookSvg} alt="facebook" style={{ width: "34px" }} />
             </a>
             <a
               href="https://www.instagram.com/mabslai/"
@@ -380,7 +386,7 @@ function OurLocation() {
               style={{ textDecoration: "none" }}
             >
               <img
-                src="/src/images/logo/instagram.svg"
+                src={instagramSvg}
                 alt="instagram"
                 style={{ width: "34px" }}
               />
@@ -390,22 +396,14 @@ function OurLocation() {
               className="me-4 text-reset"
               style={{ textDecoration: "none" }}
             >
-              <img
-                src="/src/images/logo/whatsapp.svg"
-                alt="whatsapp"
-                style={{ width: "34px" }}
-              />
+              <img src={whatsappSvg} alt="whatsapp" style={{ width: "34px" }} />
             </a>
             <a
               href="#"
               className="me-4 text-reset"
               style={{ textDecoration: "none" }}
             >
-              <img
-                src="/src/images/logo/youtube.svg"
-                alt="youtube"
-                style={{ width: "34px" }}
-              />
+              <img src={youtubeSvg} alt="youtube" style={{ width: "34px" }} />
             </a>
           </div>
         </section>
@@ -416,7 +414,7 @@ function OurLocation() {
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt-4">
                 <h6 className="text-uppercase fw-bold mb-4">
                   <img
-                    src="/src/images/mabslai.png"
+                    src={mabslaiLogo}
                     style={{
                       width: "34px",
                       height: "34px",
@@ -512,27 +510,15 @@ function OurLocation() {
                   <strong>CONTACT US</strong>
                 </h6>
                 <p>
-                  <img
-                    src="/src/images/logo/gmail.png"
-                    style={{ width: "34px" }}
-                    alt=""
-                  />
+                  <img src={email} style={{ width: "34px" }} alt="" />
                   &nbsp; mabslai@yahoo.com
                 </p>
                 <p>
-                  <img
-                    src="/src/images/logo/phone.png"
-                    style={{ width: "34px" }}
-                    alt=""
-                  />
+                  <img src={phone} style={{ width: "34px" }} alt="" />
                   &nbsp; 0916 519 1592
                 </p>
                 <p>
-                  <img
-                    src="/src/images/logo/dialphone.png"
-                    style={{ width: "34px" }}
-                    alt=""
-                  />
+                  <img src={dialphone} style={{ width: "34px" }} alt="" />
                   &nbsp; 340 3052
                 </p>
               </div>
